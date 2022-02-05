4998fcd482803ac0bc8a23de40884ded1856b79e
nicocp05 <nico_cp05@hotmail.com>
2022-02-05 01:38:35 -0300

    First commit

---
+++ src/app/pages/notpagefound/notpagefound.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notpagefound',
+  templateUrl: './notpagefound.component.html',
+  styleUrls: ['./notpagefound.component.css']
+})
+export class NotpagefoundComponent implements OnInit {
+
+  public year: number = new Date().getFullYear();
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
